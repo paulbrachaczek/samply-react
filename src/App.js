4ac05cbe291fe85ac58962158c4d029b276a3b9f
Paweł <pawel.brachaczek@enginiety.com>
2020-10-12 19:18:31 +0200

    samply app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/header/header.component'
+import Sidebar from './components/sidebar/sidebar.component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <main className="o-main">
+        <Sidebar/>
+      </main>
     </div>
   );
 }
